e989878c4700dcaf58b3e30cbd2e702c2c4fccd0
Ogzhnsfgl <o.sofuoglu@yahoo.com>
2021-09-13 12:12:37 +0300

    Fiirefox modal content doesnt appear bug fixed.

--- js/cardList.js
+++ js/cardList.js
@@ -4,26 +4,19 @@ const search = document
   .addEventListener("input", onChange);
 
 /* FETCHING DATA */
-let allData = [];
+//Reduced data store only 9 items.
+let reducedData = [];
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
   .then((data) => {
-    listItem(data);
-    allData = data;
+    //First 9 data of coming from fetch, copy to reducedData array.
+    reducedData = data.slice(0, 9);
+    listItem(reducedData);
   });
-
-function onChange(e) {
-  let input = e.target.value;
-  filteredData = allData.filter(
-    (item) => item.body.includes(input) || item.title.includes(input)
-  );
-  console.log(filteredData);
-  cardList.innerHTML = "";
-  listItem(filteredData);
-}
 
 /* LISTING ITEMS */
 const listItem = (data) => {
+  //Every items in data use to create card.
   data.forEach((item, index) => {
     const cardItem = document.createElement("div");
     cardItem.className = "card";
@@ -41,3 +34,17 @@ const listItem = (data) => {
     cardList.appendChild(cardItem);
   });
 };
+
+/* SEARCHBAR ONCHANGE */
+function onChange(e) {
+  //Get user input
+  let input = e.target.value;
+  //Create new filtered list using user input
+  filteredData = reducedData.filter(
+    (item) => item.body.includes(input) || item.title.includes(input)
+  );
+  //Clear card list
+  cardList.innerHTML = "";
+  //List filtered items
+  listItem(filteredData);
+}
--- js/formModal.js
+++ js/formModal.js
@@ -1,58 +1,72 @@
-// Get the modal
+// Get all required elements
 const modal = document.getElementById("formModal");
 const modalcontent = document.getElementById("formModal");
 const btn = document.querySelector(".form-button");
-let span = document.getElementsByClassName("close")[0];
+const formEl = document.querySelector("form").elements;
 
-// Get the button that opens the modal
+// Form button click event
 document.querySelector(".form-button").addEventListener("click", onclick);
+document.getElementsByClassName("close")[0].addEventListener("click", close);
 
-// Get the <span> element that closes the modal
+//Create new element for modal content
+const modalContent = document.createElement("div");
+modalContent.className = "formModal-content";
 
-// When the user clicks on the button, open the modal
+// Button lick function
 btn.onclick = function (e) {
   e.preventDefault();
   e.stopPropagation();
+  //Clear old data from Modal Content
   modalcontent.innerHTML = "";
-  const formEl = document.querySelector("form").elements;
-  console.log(formEl);
-  const formValue = document.createElement("div");
-  formValue.className = "formModal-content";
-  Object.keys(formEl).forEach((element) => {
-    debugger;
-    if (!parseInt(element) && element != 0 && formEl[element].value !== "") {
-      let p = document.createElement("p");
-      p.innerHTML = `<strong>${element}</strong> : ${formEl[element].value}`;
-      formValue.appendChild(p);
-    }
-  });
+  modalContent.innerHTML = "";
+  // Get form to reach form input values
+  const form = document.querySelector("form");
+  const data = new FormData(form);
+  // Iterate all key of inputs
+  for (var key of data.keys()) {
+    //Send key createItem function
+    createItem(key);
+  }
+  //Check modal content exist then open or close modal
+  if (modalContent.innerHTML !== "") {
+    chechActive();
+  } else {
+    alert("Empty form can not submit!");
+  }
+};
 
-  modal.appendChild(formValue);
+// Close button click function
+function close() {
+  chechActive();
+  // Clear form input fields
+  Object.keys(formEl).forEach((item) => {
+    formEl[item].value = "";
+  });
+}
+
+function createItem(key) {
+  // check the value of the key exist
+  if (formEl[key].value === "") {
+    return;
+  }
+  // create and fill p element
+  let p = document.createElement("p");
+  p.innerHTML = `<strong>${key}</strong> : ${formEl[key].value}`;
+  modalContent.appendChild(p);
+
+  //Append p element to modal content
+  modal.appendChild(modalContent);
+  //create close span and append it modal content
   let modalClosing = document.createElement("span");
   modalClosing.className = "close";
   modalClosing.innerHTML = `&times;`;
-
+  modalClosing.onclick = close;
   modal.appendChild(modalClosing);
+}
 
+function chechActive() {
+  //Check the state of modal
   modal.className == "formModal-active"
     ? (modal.className = "formModal")
     : (modal.className = "formModal-active");
-
-  Object.keys(formEl).forEach((element) => {
-    formEl[element].value = "";
-  });
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.className == "formModal-active"
-    ? (modal.className = "formModal")
-    : (modal.className = "formModal-active");
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target !== modal && event.target !== btn) {
-    modal.className = "formModal";
-  }
-};
+}
